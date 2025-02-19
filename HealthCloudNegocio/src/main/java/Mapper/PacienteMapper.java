11cc63a93d6cc27d384ea1927ace6ae5e6636d79
FreddyACastroR <freddy.castro252191@potros.itson.edu.mx>
2025-02-19 13:52:55 +0000

    Capa Negocio (PacienteNuevoDTO)

---
+++ HealthCloudNegocio/src/main/java/Mapper/PacienteMapper.java
@@ -0,0 +1,29 @@
+package Mapper;
+
+import DTO.PacienteNuevoDTO;
+import Entidades.Paciente;
+
+public class PacienteMapper {
+
+    /**
+     * Es necesario agregar la dependencia de Persistencia.
+     * Realizar Clean and Build de HealthCloudPersistencia y agregar en Dependencia de HealthCloudNegocio.
+     * Por ultimo realizar import necesario.
+     */  
+
+    // Sale error por falta de Dependencia de Persistencia.
+    puublic Paciente toEntity (PacienteNuevoDTO pacienteNuevo) {
+        Paciente paciente = new Paciente(pacienteNuevo.getNombrePila(), pacienteNuevo.getApellidoPaterno(), 
+        pacienteNuevo.getApellidoMaterno(), pacienteNuevo.getCorreoElectronico(), pacienteNuevo.getNumTelefono(),
+        pacienteNuevo.getContrasenia(), pacienteNuevo.getFechaNacimiento(), pacienteNuevo.getDireccion());
+        return paciente;
+    }
+
+    public PacienteNuevoDTO toDTO (Paciente paciente) {
+        PacienteNuevoDTO pacienteDTO = new PacienteNuevoDTO(paciente.getNombrePila(), paciente.getApellidoPaterno(),
+        paciente.getApellidoMaterno(), paciente.getCorreoElectronico(), paciente.getNumTelefono(), paciente.getContrasenia(),
+        paciente.getFechaNacimiento(), paciente.getDireccion());
+        return pacienteDTO;
+    }
+
+}
