9d6163940996db891125d49143761697f7394783
MarisolRuizP <marisol.ruiz253120@potros.itson.edu.mx>
2025-02-18 19:08:55 -0600

    Proyectos

--- HealthCloudPresentacion/src/main/java/com/mycompany/healthcloudpresentacion/HealthCloudPresentacion.java
+++ HealthCloudPresentacion/src/main/java/com/mycompany/healthcloudpresentacion/HealthCloudPresentacion.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package error.healthcloudnegocio;
+package com.mycompany.healthcloudpresentacion;
 
 /**
  *
  * @author Maryr
  */
-public class HealthCloudNegocio {
+public class HealthCloudPresentacion {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
