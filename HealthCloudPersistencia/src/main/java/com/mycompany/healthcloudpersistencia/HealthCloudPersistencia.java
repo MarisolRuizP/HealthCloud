c15b04831aee53f9f04baf210ae3878ac511d76a
JP3up <jrascon51@gmail.com>
2025-02-18 20:35:08 -0700

    primer commit

--- HealthCloudPersistencia/src/main/java/com/mycompany/healthcloudpersistencia/HealthCloudPersistencia.java
+++ HealthCloudPersistencia/src/main/java/com/mycompany/healthcloudpersistencia/HealthCloudPersistencia.java
@@ -11,6 +11,6 @@ package com.mycompany.healthcloudpersistencia;
 public class HealthCloudPersistencia {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello nada");
     }
 }
