1a1cfb40df0edd8e64f49c5b4f404bdf3e29d28a
MarisolRuizP <marisol.ruiz253120@potros.itson.edu.mx>
2025-02-18 18:41:15 -0600

    Connect y asi

---
+++ HealthCloudPresentacion/src/main/java/error/healthcloudpresentacion/HealthCloudPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package error.healthcloudpresentacion;
+
+/**
+ *
+ * @author Maryr
+ */
+public class HealthCloudPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
