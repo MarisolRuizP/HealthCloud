6f0abe024fe474c36785e4431546411020c800dd
JP3up <jrascon51@gmail.com>
2025-02-22 20:56:48 -0700

    Se intento hacer la funcionalidad pero no se concreto, solo se creo la tabla para que se puedan mostrar las citas del paciente

--- HealthCloudPresentacion/src/main/java/GUI/FrmHistorial.java
+++ HealthCloudPresentacion/src/main/java/GUI/FrmHistorial.java
@@ -4,6 +4,16 @@
  */
 package GUI;
 
+import BO.HistorialCitaBO;
+import Conexion.ConexionBD;
+import Conexion.IConexionBD;
+import Entidades.Cita;
+import Entidades.Paciente;
+import Exception.NegocioException;
+import java.awt.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Maryr
@@ -15,6 +25,38 @@ public class FrmHistorial extends javax.swing.JFrame {
      */
     public FrmHistorial() {
         initComponents();
+
+        HistorialCitaBO historialCitaBO;
+
+        IConexionBD conexion = new ConexionBD(); // Conexión a la BD
+        historialCitaBO = new HistorialCitaBO(conexion);
+
+        cargarHistorialCitas();
+    }
+
+    private void cargarHistorialCitas() {
+        DefaultTableModel modeloTabla = (DefaultTableModel) jTable1.getModel();
+
+        modeloTabla.setRowCount(0);
+
+        try {
+            
+            List<Cita> historial = historialCitaBO.obtenerHistorialCitas(paciente);
+
+            for (Cita cita : historial) {
+                modeloTabla.addRow(new Object[]{
+                    cita.getFolioEmergencia(),
+                    cita.getFecha(),
+                    cita.getHora(),
+                    cita.getMotivo(),
+                    cita.getEstadoCita()
+                });
+            }
+
+        } catch (NegocioException e) {
+            JOptionPane.showMessageDialog(this, "Error al cargar las citas, " + e.getMessage(),
+                    "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     /**
@@ -26,21 +68,238 @@ public class FrmHistorial extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        BtnCitasSide = new javax.swing.JButton();
+        BtnInfoSide = new javax.swing.JButton();
+        BtnHistorialSide = new javax.swing.JButton();
+        BtnCitaEmSide = new javax.swing.JButton();
+        BtnInicio = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(106, 154, 176));
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+
+        BtnCitasSide.setBackground(new java.awt.Color(58, 109, 140));
+        BtnCitasSide.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
+        BtnCitasSide.setForeground(new java.awt.Color(0, 0, 0));
+        BtnCitasSide.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/book_closed_icon_246534.png"))); // NOI18N
+        BtnCitasSide.setText("Citas");
+        BtnCitasSide.setBorder(null);
+        BtnCitasSide.setHorizontalAlignment(javax.swing.SwingConstants.LEADING);
+        BtnCitasSide.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                BtnCitasSideMouseClicked(evt);
+            }
+        });
+        BtnCitasSide.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnCitasSideActionPerformed(evt);
+            }
+        });
+
+        BtnInfoSide.setBackground(new java.awt.Color(58, 109, 140));
+        BtnInfoSide.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
+        BtnInfoSide.setForeground(new java.awt.Color(0, 0, 0));
+        BtnInfoSide.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/4092564-about-mobile-ui-profile-ui-user-website_114033.png"))); // NOI18N
+        BtnInfoSide.setText("Información personal");
+        BtnInfoSide.setBorder(null);
+        BtnInfoSide.setHorizontalAlignment(javax.swing.SwingConstants.LEADING);
+        BtnInfoSide.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                BtnInfoSideMouseClicked(evt);
+            }
+        });
+        BtnInfoSide.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnInfoSideActionPerformed(evt);
+            }
+        });
+
+        BtnHistorialSide.setBackground(new java.awt.Color(58, 109, 140));
+        BtnHistorialSide.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
+        BtnHistorialSide.setForeground(new java.awt.Color(0, 0, 0));
+        BtnHistorialSide.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/calendar_date_day_event_month_schedule_time_icon_123230.png"))); // NOI18N
+        BtnHistorialSide.setText("Historial");
+        BtnHistorialSide.setBorder(null);
+        BtnHistorialSide.setHorizontalAlignment(javax.swing.SwingConstants.LEADING);
+        BtnHistorialSide.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                BtnHistorialSideMouseClicked(evt);
+            }
+        });
+        BtnHistorialSide.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnHistorialSideActionPerformed(evt);
+            }
+        });
+
+        BtnCitaEmSide.setBackground(new java.awt.Color(58, 109, 140));
+        BtnCitaEmSide.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
+        BtnCitaEmSide.setForeground(new java.awt.Color(0, 0, 0));
+        BtnCitaEmSide.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/-weather-lightning_98198.png"))); // NOI18N
+        BtnCitaEmSide.setText("Cita emergencia");
+        BtnCitaEmSide.setBorder(null);
+        BtnCitaEmSide.setHorizontalAlignment(javax.swing.SwingConstants.LEADING);
+        BtnCitaEmSide.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                BtnCitaEmSideMouseClicked(evt);
+            }
+        });
+        BtnCitaEmSide.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnCitaEmSideActionPerformed(evt);
+            }
+        });
+
+        BtnInicio.setBackground(new java.awt.Color(58, 109, 140));
+        BtnInicio.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
+        BtnInicio.setForeground(new java.awt.Color(0, 0, 0));
+        BtnInicio.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/3844470-home-house_110332.png"))); // NOI18N
+        BtnInicio.setBorder(null);
+        BtnInicio.setContentAreaFilled(false);
+        BtnInicio.setHorizontalAlignment(javax.swing.SwingConstants.LEADING);
+        BtnInicio.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                BtnInicioMouseClicked(evt);
+            }
+        });
+        BtnInicio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnInicioActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(42, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(BtnCitaEmSide, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BtnHistorialSide, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BtnInfoSide, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BtnCitasSide, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(45, 45, 45))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(BtnInicio)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(BtnInfoSide, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30)
+                .addComponent(BtnCitasSide, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31)
+                .addComponent(BtnHistorialSide, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addComponent(BtnCitaEmSide, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(57, 57, 57)
+                .addComponent(BtnInicio, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {},
+            new String [] {
+                "Folio", "Fecha", "Hora", "Motivo", "Estado"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(30, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(423, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BtnCitasSideMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnCitasSideMouseClicked
+        setVisible(false);
+        FrmCitasPaciente frmCitas = new FrmCitasPaciente();
+        frmCitas.setVisible(true);
+    }//GEN-LAST:event_BtnCitasSideMouseClicked
+
+    private void BtnCitasSideActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnCitasSideActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BtnCitasSideActionPerformed
+
+    private void BtnInfoSideMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnInfoSideMouseClicked
+        setVisible(false);
+        FrmInfoPersPaciente frmInfoPerPac = new FrmInfoPersPaciente(identificador);
+        frmInfoPerPac.setVisible(true);
+    }//GEN-LAST:event_BtnInfoSideMouseClicked
+
+    private void BtnInfoSideActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnInfoSideActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BtnInfoSideActionPerformed
+
+    private void BtnHistorialSideMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnHistorialSideMouseClicked
+        setVisible(false);
+        FrmHistorial frmHistorial = new FrmHistorial();
+        frmHistorial.setVisible(true);
+    }//GEN-LAST:event_BtnHistorialSideMouseClicked
+
+    private void BtnHistorialSideActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnHistorialSideActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BtnHistorialSideActionPerformed
+
+    private void BtnCitaEmSideMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnCitaEmSideMouseClicked
+        setVisible(false);
+        FrmCitaEmergencia frmCitaEm = new FrmCitaEmergencia(identificador);
+        frmCitaEm.setVisible(true);
+    }//GEN-LAST:event_BtnCitaEmSideMouseClicked
+
+    private void BtnCitaEmSideActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnCitaEmSideActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BtnCitaEmSideActionPerformed
+
+    private void BtnInicioMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnInicioMouseClicked
+        setVisible(false);
+        FrmInicioPaciente frmInicio = new FrmInicioPaciente(identificador);
+        frmInicio.setVisible(true);
+    }//GEN-LAST:event_BtnInicioMouseClicked
+
+    private void BtnInicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnInicioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BtnInicioActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +338,14 @@ public class FrmHistorial extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BtnCitaEmSide;
+    private javax.swing.JButton BtnCitasSide;
+    private javax.swing.JButton BtnHistorialSide;
+    private javax.swing.JButton BtnInfoSide;
+    private javax.swing.JButton BtnInicio;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
